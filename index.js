674aca610bbe5062841daa0d111db54c3dff5170
kiendao515 <daotrungkien515@gmail.com>
2021-11-08 00:01:57 +0700

    dhhdhdd

--- index.js
+++ index.js
@@ -4,18 +4,22 @@ const userRoute= require('./routes/userRoute');
 var cors = require('cors');
 const app = express();
 connectionDB();
+app.use(express.json());
 app.use(express.urlencoded({extended:false}))
 
-app.use(cors());
-app.options('*', cors());
-app.all('/*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+const corsOpts = {
+    origin: '*',
+    methods: [
+        'GET',
+        'POST',
+    ],
 
+    allowedHeaders: [
+        'Content-Type',
+    ],
+};
+
+app.use(cors(corsOpts));
 app.use(userRoute,function(req,res,next){
     next()
 });
