905dd10bf298c16d105ec7c050c3c6ebbefb9701
kiendao515 <daotrungkien515@gmail.com>
2021-12-08 09:45:31 +0700

    dhdhdhdhd

--- routes/bikeRoute.js
+++ routes/bikeRoute.js
@@ -9,7 +9,7 @@ router.get("/api/v2/bike/detail/:id",bikeController.getDetailBike)
 router.post("/api/v2/bike/add",checkAdminRole,bikeController.addBike);
 router.put("/api/v2/bike/edit/:id",checkAdminRole,bikeController.editBike);
 router.delete("/api/v2/bike/delete/:id",checkAdminRole,bikeController.deleteBike);
-router.get("/api/v2/station/free",checkAdminRole,bikeController.getStationWithFreeBike);
+router.get("/api/v2/station/free",bikeController.getStationWithFreeBike);
 router.get("/api/v2/statistics/:id",checkAdminRole,bikeController.getStatistics);
 router.get("/api/v2/bikes/free/:id",checkAdminRole,bikeController.getFreeBikeFromStation);
 
